ee188f26b8d13adfd9fb830f23dfbadf65fa9a11
Bikram48 <chandbikram001@gmail.com>
2023-07-02 19:18:27 +0545

    complete - banner section for both mobile and desktop version

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,6 +1,29 @@
 import React from "react";
+import BannerImg from "../../assets/cherrydeck.jpeg";
 import "./banner.css";
 
 export default function Banner() {
-  return <div></div>;
+  return (
+    <div className="raven__banner">
+      <div className="raven__banner-content">
+        <div className="raven__banner-content_title">
+          <h1>WE ARE DIGITAL AGENCY</h1>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit id
+          fringilla turpis tempor enim. Vel tristique suspendisse eget sodales
+          egestas. Donec duis lacus, odio velit eu eget ac tortor. Ac sit
+          ultricies hendrerit pellentesque arcu fames sit lectus. Scelerisque id
+          bibendum velit cras.{" "}
+        </p>
+        <div className="raven__banner-content_btn">
+          <button type="button">Learn more</button>
+        </div>
+      </div>
+
+      <div className="raven__banner-image">
+        <img src={BannerImg} alt="" />
+      </div>
+    </div>
+  );
 }
