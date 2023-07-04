7f53e45a2b38aaa83076a1d87be5dadc5fa897a1
Bikram48 <chandbikram001@gmail.com>
2023-07-04 16:06:30 +0545

    implement - clients component

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,6 +1,36 @@
 import React from "react";
 import "./clients.css";
+import { mango, microsoft, nike, amazon } from "./imports";
 
 export default function Clients() {
-  return <div></div>;
+  return (
+    <div className="raven__clients section__padding container">
+      <div className="raven__clients-title title__margin">
+        <h3>Clients</h3>
+      </div>
+      <div className="raven__clients-images">
+        <div>
+          <img src={mango} alt="mango" />
+        </div>
+        <div>
+          <img src={microsoft} alt="microsoft" />
+        </div>
+        <div>
+          <img src={nike} alt="nike" />
+        </div>
+        <div>
+          <img src={amazon} alt="dropbox" />
+        </div>
+        <div>
+          <img src={mango} alt="mango" />
+        </div>
+        <div>
+          <img src={microsoft} alt="microsoft" />
+        </div>
+        <div>
+          <img src={nike} alt="nike" />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/clients/imports.js
@@ -0,0 +1,6 @@
+import mango from "../../assets/mango.png";
+import microsoft from "../../assets/microsoft.png";
+import nike from "../../assets/nike.png";
+import amazon from "../../assets/amazon.png";
+
+export { mango, microsoft, nike, amazon };
