54d1d8f7311808c92d2d055c42a157bf9d7d6d2e
Bikram48 <chandbikram001@gmail.com>
2023-07-04 12:47:43 +0545

    complete - team component design for both mobile and desktop

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./team.css";
 import { john, timmy, dmitry } from "./imports.js";
+import TeamMember from "./team-member/TeamMember";
 
 export default function Team() {
-  return <div></div>;
+  return (
+    <div className="raven__team section__padding container">
+      <div className="raven__team-title title__margin">
+        <h3>Our Team</h3>
+      </div>
+
+      <div className="raven__team-contents">
+        <TeamMember image={john} />
+        <TeamMember type="text" />
+        <TeamMember image={timmy} />
+        <TeamMember image={dmitry} />
+      </div>
+    </div>
+  );
 }
--- src/components/team/team-member/TeamMember.js
+++ src/components/team/team-member/TeamMember.js
@@ -1,6 +1,31 @@
 import React from "react";
 import "./member.css";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
-export default function TeamMember() {
-  return <div></div>;
+export default function TeamMember({ image, type }) {
+  return (
+    <>
+      {type ? (
+        <div className="raven__team-ceo_description">
+          <h4 className="name">Jane Doe</h4>
+          <p className="position">Company/CEO</p>
+          <p className="description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel eius
+            id molestias autem, consequatur nulla voluptatibus architecto
+            temporibus error quis?
+          </p>
+
+          <div className="social__icons">
+            <FaFacebook />
+            <FaTwitter />
+            <FaInstagram />
+          </div>
+        </div>
+      ) : (
+        <div className="raven__team-image">
+          <img src={image} alt="" />
+        </div>
+      )}
+    </>
+  );
 }
